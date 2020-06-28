1066a6ac36b2b7db75c5e362fc87efd46d96c372
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2020-06-28 13:05:16 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+
+
+
+export const getGifs = async (category) => {
+
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=Y3sQL5q2C62I3f3yL7dCOJ8FcnmNv3H1`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    }
+  })
+
+  return gifs;
+}
